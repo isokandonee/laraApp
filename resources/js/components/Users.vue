7a861ea4b80f8dd05cc453d696fd5c903d0d2fbb
Ogunniyi Isokan Mitchel <isokandone@gmail.com>
2020-05-11 21:31:10 +0100

    modal was added to the user component

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -1,58 +1,74 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Users</h3>
-                        <div class="card-tools">
-                            <div class="input-group input-group-sm" style="width:150px;" >
-                                <input type="text" name="table_search" placeholder="Search..." 
-                                    class="form-control float-right">
-                                <div class="input-group-append">
-                                    <button type="submit" class="fa fa-search" class="btn btn-default"></button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="card-body table-responsive p-0">
-                        <table class="table table-hover">
-                            <tbody>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>User</th>
-                                    <th>Date</th>
-                                    <th>Status</th>
-                                    <th>Reason</th>
-                                </tr>
-                                <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-07-2014</td>
-                                    <td><span class="tag tag-success">Pending</span></td>
-                                    <td>ipsum dolor sit amet jndfvnkjkjfdjbv;a jkjdkcb ksjdkj kjsbdkjk</td>
-                                </tr>
-                                <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-07-2014</td>
-                                    <td><span class="tag tag-success">Approved</span></td>
-                                    <td>ipsum dolor sit amet jndfvnkjkjfdjbv;a jkjdkcb ksjdkj kjsbdkjk</td>
-                                </tr>
-                                <tr>
-                                    <td>183</td>
-                                    <td>John Doe</td>
-                                    <td>11-07-2014</td>
-                                    <td><span class="tag tag-success">Denied</span></td>
-                                    <td>ipsum dolor sit amet jndfvnkjkjfdjbv;a jkjdkcb ksjdkj kjsbdkjk</td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- .card-body -->
-                </div>
+        <div class="row mt-5">
+            <div class="col-md-12">
                 <!-- .card -->
+                <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Responsive Hover Table</h3>
+
+                <div class="card-tools">
+                  <button class="btn btn-success" data-toggle="modal" data-target="#addNew">
+                      Add New
+                      <i class="fas fa-user-plus fa-fw"></i>
+                  </button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Type</th>
+                      <th>Modify</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>183</td>
+                      <td>John Doe</td>
+                      <td>11-7-2014</td>
+                      <td><span class="tag tag-success">Approved</span></td>
+                      <td>
+                          <a href="#">
+                              <i class="fa fa-edit"></i>
+                          </a>
+                          ||
+                          <a href="#">
+                              <i class="fa fa-trash red"></i>
+                          </a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+                <!-- .card -->
+            </div>
+        </div>
+
+        <!-- Modal -->
+        <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="addNewLabel">Add New User</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+                </div>
             </div>
         </div>
     </div>
